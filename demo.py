7196a54a6c66b40b7f9d3b83938df7b7a4f99dd7
laffitte-csgroup <laffitte-csgroup>
2022-02-23 17:31:44 +0100

    test prefect cloud

--- demo.py
+++ demo.py
@@ -1,19 +1,38 @@
-import prefect
+from typing import List
+
 from prefect import task, Flow, Parameter
 from prefect.storage import GitHub
 from prefect.run_configs import UniversalRun
 
+from prefect_wfs.algorithms.common.string import *
+
 @task
-def hello_world(name):
-  logger = prefect.context.get("logger")
-  logger.info("Hello "+name)
+def task_replace(a: List[str], old_value: str, new_value: str):
+  return replace(a, old_value, new_value)
 
-with Flow("demo_pr_github") as flow:
-  name = Parameter('name', default = 'Bob')
-  hello_world(name)
+@task
+def task_split(a: List[str]):
+  return split(a)
 
-flow.storage = GitHub(repo="laffitte-csgroup/demo_pr", path="/demo.py", stored_as_script=True)
+@task
+def task_concat(a: List[str]):
+  return concat(a)
 
-flow.run_config = UniversalRun(labels=['sde-scheduling'])
+a = ["bananes", "pommes", "ananas", "poires", "mangues", "kiwis"]
 
-flow.register(project_name="demo_s1")
+with Flow("demo_workflow_1") as flow1:
+  x = task_replace(a, "bananes", "coconuts")
+  x = task_split(x)
+  x = task_concat(x)
+  print(x)
+
+with Flow("demo_workflow_2") as flow2:
+  x = task_replace(a, "bananes", "coconuts")
+  x = task_concat(x)
+  x = task_split(x)
+  print(x)
+
+for flow in [flow1, flow2]:
+    flow.storage = GitHub(repo="laffitte-csgroup/demo_pr", path="/demo.py", stored_as_script=True)
+    #flow.run_config = UniversalRun(labels=['sde-scheduling'])
+    flow.register(project_name="demo")

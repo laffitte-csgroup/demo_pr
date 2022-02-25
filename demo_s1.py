731657ba85bbfbd6e9ce68ee54facb69211ea180
laffitte-csgroup <94380756+laffitte-csgroup@users.noreply.github.com>
2022-02-25 11:29:30 +0100

    Update demo_s1.py

--- demo_s1.py
+++ demo_s1.py
@@ -39,13 +39,13 @@ def task_print(a: List[str]):
 lorem_ipsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 a = lorem_ipsum.split(' ')
 
-with Flow("demo_workflow_1") as flow1:
+with Flow("demo_workflow_github_1") as flow1:
   x = task_replace(a, "bananes", "coconuts")
   x = task_split(x)
   x = task_concat(x)
   task_print(x)
 
-with Flow("demo_workflow_2") as flow2:
+with Flow("demo_workflow_github_2") as flow2:
   x = task_replace(a, "bananes", "coconuts")
   x = task_concat(x)
   x = task_split(x)
